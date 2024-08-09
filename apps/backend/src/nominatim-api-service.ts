46603bc453298bdfbfba0a1b6425f686fed5fd76
Ícaro <icaroribeiro@hotmail.com>
2024-08-09 18:40:14 -0300

    Handle environment variables

---
+++ apps/backend/src/nominatim-api-service.ts
@@ -0,0 +1,58 @@
+import {
+  ApplicationSettings,
+  getNominatimAPIURL,
+} from "./application-settings";
+
+type AddressParams = {
+  amenity: string;
+  street: string;
+  city: string;
+  county: string;
+  state: string;
+  country: string;
+  postalcode: string;
+};
+
+type GeoPointCoordinates = {
+  lat: number;
+  long: number;
+};
+
+class NominatimAPIService {
+  private baseUrl: string | undefined;
+
+  constructor() {
+    const applicationSettings = new ApplicationSettings();
+    this.baseUrl = applicationSettings.getNominatimAPIURL();
+  }
+
+  public async getGeoPointCoordinates(
+    addressParams: AddressParams,
+  ): Promise<GeoPointCoordinates | undefined> {
+    const urlQuery = this.createSearchURLQuery(addressParams);
+    const url = `${this.baseUrl}/search?${urlQuery}&format=geojson`;
+    try {
+      const response = await fetch(url, {
+        method: "GET",
+        headers: { "content-Type": "application/json" },
+      });
+      const coordinates = this.parseGeoPointCoordinates(response);
+      console.log(coordinates);
+      return coordinates;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  private createSearchURLQuery(addressParams: AddressParams): string {
+    console.log(addressParams);
+    return "";
+  }
+
+  private parseGeoPointCoordinates(response: Response): GeoPointCoordinates {
+    console.log(response);
+    return { lat: 0, long: 0 };
+  }
+}
+
+export { NominatimAPIService, type AddressParams };
