777c1bdf9dfc2ec1b77fec301537e831f2fc7711
Ícaro <icaroribeiro@hotmail.com>
2024-08-09 15:19:52 -0300

    Initialize backend API

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from "express";
+
+const PORT = process.env.PORT || 8000;
+
+const app: Application = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    message: "hello world",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log("server started at http://localhost:" + PORT);
+});
