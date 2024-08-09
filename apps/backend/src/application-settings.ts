d2dcffa4c90c5dd5d87b4d642b335b5669472981
Ícaro <icaroribeiro@hotmail.com>
2024-08-09 18:55:38 -0300

    Update application settings

--- apps/backend/src/application-settings.ts
+++ apps/backend/src/application-settings.ts
@@ -1,16 +1,16 @@
 class ApplicationSettings {
-  public getPort() {
+  public getPort(): string {
     return this.getEnvVar("PORT");
   }
 
-  public getNominatimAPIURL() {
+  public getNominatimAPIURL(): string {
     return this.getEnvVar("NOMINATIM_API_URL");
   }
 
-  private getEnvVar(name: string): string | undefined {
+  private getEnvVar(name: string): string {
     if (!process.env[name]) {
       console.warn(`${name} isn't set`);
-      return undefined;
+      return "";
     }
     return process.env[name];
   }
