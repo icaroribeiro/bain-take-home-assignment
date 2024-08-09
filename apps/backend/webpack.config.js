d1c4c80f0f76f605a508de6e99820400f586cffe
Ícaro <icaroribeiro@hotmail.com>
2024-08-09 15:20:45 -0300

    Add changes to bundle backend API

--- apps/backend/webpack.config.js
+++ apps/backend/webpack.config.js
@@ -1,7 +1,23 @@
 const path = require("path");
 
+const { NODE_ENV = "production" } = process.env;
+
 module.exports = {
   entry: "./src/index.ts",
+  mode: NODE_ENV,
+  target: "node",
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
   output: {
     filename: "index.js",
     path: path.resolve(__dirname, "dist"),
