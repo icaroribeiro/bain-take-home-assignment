4644575a77d436726bddebb15a32385b6c4b90b6
Ícaro <icaroribeiro@hotmail.com>
2024-08-10 22:26:56 -0300

    Add plugin to sort imports and exports

--- apps/backend/eslint.config.mjs
+++ apps/backend/eslint.config.mjs
@@ -1,6 +1,7 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 export default tseslint.config(
   eslint.configs.recommended,
@@ -17,9 +18,14 @@ export default tseslint.config(
     ignores: ["eslint.config.mjs", "webpack.config.js"],
   },
   {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
     rules: {
       "@typescript-eslint/consistent-type-definitions": ["error", "type"],
-      "@typescript-eslint/no-misused-promises": ["error", "type"],
+      "@typescript-eslint/no-misused-promises": "error",
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
     },
   },
 );
