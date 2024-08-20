939e826bddba8049bdec074686d602e3f095e78c
Ícaro <icaroribeiro@hotmail.com>
2024-08-19 22:12:07 -0300

    Update backend app postgres datasource

--- apps/backend/src/data-sources/postgres.data-source.ts
+++ apps/backend/src/data-sources/postgres.data-source.ts
@@ -2,14 +2,15 @@ import "reflect-metadata";
 
 import { DataSource } from "typeorm";
 
-import { AddrsDistanceHistory, Model } from "../entities";
+import { DistsBtwAddrsHistEntity as AddressesDistanceCalc } from "../api/entities";
+import { BaseEntity as Base } from "../api/entities";
 import {
   getPostgresDBHost,
   getPostgresDBName,
   getPostgresDBPassword,
   getPostgresDBPort,
   getPostgresDBUser,
-} from "../utils";
+} from "../config/postgres-database";
 
 const postgresConfig = {
   host: getPostgresDBHost(),
@@ -22,9 +23,9 @@ const postgresConfig = {
 const PostgresDataSource = new DataSource({
   ...postgresConfig,
   type: "postgres",
-  synchronize: false,
+  synchronize: true,
   logging: false,
-  entities: [Model, AddrsDistanceHistory],
+  entities: [Base, AddressesDistanceCalc],
 });
 
 export { PostgresDataSource };
